71a641d356b182117d4ec7bdfbdfe48114f7845f
Oskar Grunning <oskargrunning@gmail.com>
2016-04-06 06:46:52 +0200

    The writeToCache option should be respected when trying to store a model in the modelStore.

--- shared/base/model.js
+++ shared/base/model.js
@@ -41,7 +41,8 @@ var BaseModel = Backbone.Model.extend({
    * Instance method to store in the modelStore.
    */
   store: function() {
-    if (this.id !== undefined && this.app && this.app.fetcher) {
+    if (this.id !== undefined && this.options && this.options.writeToCache &&
+      this.app && this.app.fetcher) {
       this.app.fetcher.modelStore.set(this);
     }
   }
